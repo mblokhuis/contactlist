6398b17eb68aade159ce739ddc0a636c77020c1b
Martijn Blokhuis <blokhuis.m@gmail.com>
2018-12-20 14:55:05 +0100

    add funcion added

--- js/sript.js
+++ js/sript.js
@@ -1,9 +1,31 @@
-function voegIemandToe() {   
+function voegIemandToe() { 
+  
         var node = document.createElement("li");
+        
+        document.getElementById("myList").appendChild(node);
+        var div1 = document.createElement("DIV");
+        div1.className="card";
+        node.appendChild(div1);
+        var div2 = document.createElement("DIV");
+        div2.className="card-body";
+        div1.appendChild(div2);
+        var hNode = document.createElement("H4");
+        hNode.className="card-title";
         var sNaam = document.getElementById("myNewName").value;
-        var textnode = document.createTextNode(sNaam);
-        node.appendChild(textnode);            
-    document.getElementById("myList").appendChild(node);
+        hNode.innerText=sNaam;
+        div2.appendChild(hNode);
+        var pNode = document.createElement("P");
+        pNode.className="card-text";
+        pNode.innerText="Some quick example text to build on the card title and make up the bulk of the card's content.";
+        div2.appendChild(pNode);
+        var aNode = document.createElement("A");
+        aNode.className="btn btn-primary";
+        aNode.href="#!";
+        aNode.innerText="More Info";
+        pNode.appendChild(aNode);
+        
+                   
+    
 }
     
 function myFilterFunction() {
